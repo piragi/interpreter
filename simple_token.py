aeb11766abda1f3e6db1b8f115cd553b890271d5
Julius Sula <julius.sula@gmail.com>
2023-12-24 11:35:01 +0100

    tokenizer

--- simple_token.py
+++ simple_token.py
@@ -1,7 +1,7 @@
 ILLEGAL = "ILLEGAL"
 EOF = "EOF"
 # identifiers
-IDENT = "IDENTIFIER"
+IDENT = "IDENT"
 INT = "INT"
 # operators
 ASSIGN = "="
@@ -17,6 +17,7 @@ RBRACE = "}"
 FUNCTION = "FUNCTION"
 LET = "LET"
 
+KEYWORDS = {"fn": FUNCTION, "let": LET}
 
 class Lexer:
     def __init__(self, input:str):
@@ -29,13 +30,15 @@ class Lexer:
 
     def read_char(self):
         if self.read_position >= len(self.input):
-            self.char = 0
+            self.char = ""
             return
         self.char = self.input[self.read_position]
         self.position = self.read_position
         self.read_position += 1
 
     def next_token(self):
+        self.skip_whitespaces()
+
         if self.char == "=":
             token = Token("ASSIGN", "=")
         elif self.char == "+":
@@ -52,10 +55,42 @@ class Lexer:
             token = Token("COMMA", ",")
         elif self.char == ";":
             token = Token("SEMICOLON", ";")
-        else:
+        elif self.char == "":
             token = Token("EOF", "")
+        else:
+            if self.char.isalpha():
+                literal = self.read_literal()
+                # TODO: this return is weird
+                type = self.check_keyword(literal)
+                return Token(type, literal)
+            elif self.char.isnumeric():
+                literal = self.read_number()
+                return Token(INT, literal) 
+            else:
+                token = Token("ILLEGAL", self.char)
         self.read_char()
         return token
+
+    def read_literal(self):
+        literal_start = self.position
+        while self.char.isalpha() or self.char == "_":
+            self.read_char()
+        return self.input[literal_start:self.position]
+    
+    def read_number(self):
+        literal_start = self.position
+        while self.char.isnumeric():
+            self.read_char()
+        return self.input[literal_start:self.position]
+    
+    def check_keyword(self, literal):
+        if literal in KEYWORDS:
+            return KEYWORDS[literal]
+        return IDENT
+
+    def skip_whitespaces(self):
+        while self.char.isspace():
+            self.read_char()
 
 class Token:
     def __init__(self, type: str, literal: str):
--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -4,16 +4,68 @@ from pathlib import Path
 root_dir = Path(__file__).parent.parent
 sys.path.append(str(root_dir))
 
-import simple_token
+from simple_token import Lexer, Token
 
-def test_next_token():
-    input = "=+(){},;"
-    assert_input = ["ASSIGN", "PLUS", "LPAREN", "RPAREN", "LBRACE", "RBRACE", "COMMA", "SEMICOLON"]
-
-
-    lexer = simple_token.Lexer(input)
-    print(lexer.char)
+def assert_tokens(input, assert_input):
+    lexer = Lexer(input)
 
     for assert_token in assert_input:
         token = lexer.next_token()
-        assert token.type == assert_token
+        assert token.type == assert_token.type
+        assert token.literal == assert_token.literal
+
+def test_operators():
+    input = "=+(){},;"
+    assert_input = [Token("ASSIGN", "="), Token("PLUS", "+"), Token("LPAREN", "("), Token("RPAREN", ")"), Token("LBRACE", "{"), Token("RBRACE", "}"), Token("COMMA", ","), Token("SEMICOLON", ";")]
+    assert_tokens(input, assert_input)
+
+def test_add():
+    input = """let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);"""
+    assert_input = [Token("LET", "let"),
+        Token("IDENT", "five"),
+        Token("ASSIGN", "="),
+        Token("INT", "5"),
+        Token("SEMICOLON", ";"),
+        Token("LET", "let"),
+        Token("IDENT", "ten"),
+        Token("ASSIGN", "="),
+        Token("INT", "10"),
+        Token("SEMICOLON", ";"),
+        Token("LET", "let"),
+        Token("IDENT", "add"),
+        Token("ASSIGN", "="),
+        Token("FUNCTION", "fn"),
+        Token("LPAREN", "("),
+        Token("IDENT", "x"),
+        Token("COMMA", ","),
+        Token("IDENT", "y"),
+        Token("RPAREN", ")"),
+        Token("LBRACE", "{"),
+        Token("IDENT", "x"),
+        Token("PLUS", "+"),
+        Token("IDENT", "y"),
+        Token("SEMICOLON", ";"),
+        Token("RBRACE", "}"),
+        Token("SEMICOLON", ";"),
+        Token("LET", "let"),
+        Token("IDENT", "result"),
+        Token("ASSIGN", "="),
+        Token("IDENT", "add"),
+        Token("LPAREN", "("),
+        Token("IDENT", "five"),
+        Token("COMMA", ","),
+        Token("IDENT", "ten"),
+        Token("RPAREN", ")"),
+        Token("SEMICOLON", ";"),
+        Token("EOF", ""),
+    ]
+
+    assert_tokens(input, assert_input)
+
